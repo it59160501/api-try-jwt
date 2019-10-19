117695c27b3ebc3abf8d65a9bc66ed2626aba5d8
Pimped Toloy <59160501@go.buu.ac.th>
2019-10-19 09:55:11 +0700

    API try-jwt v.2

--- server.js
+++ server.js
@@ -1,8 +1,45 @@
 const express = require("express")
-const app = express()
+const mongodb = require('mongodb')
 
+const app = express()
 const port = process.env.PORT
+const MongoClient = mongodb.MongoClient
+const MongoURL = process.env.MONGODB_URL
+const options = { useNewUrlParser : true, useUnifiedTopology : true}
+const database = "buu"
+const collection = "users"
+
+app.use(express.json())
+
+app.post('/register', async (req, res)=>{
+    let name = req.body.name
+    let email = req.body.email
+    let studentId = req.body.studentId
+    let password = req.body.password
+
+    const o = {
+        name:name,
+        email:email,
+        studentId:studentId
+    }
+
+    const client = await MongoClient.connect( MongoURL, options ).catch((err) => {
+        console.log(`Cannot connect to Mongo  ${err}`)
+        res.status(500).json({error:err})
+        return
+    })
+
+    let db = client.db(database)
+    let result = await db.collection(collection).insertOne(o).catch(err => {
+        console.log(`Cannot insert data to users collection : ${err}`)
+        res.status(500).json({error:err})
+        return
+    })
+
+    res.status(201).json(o)
+})
+
 
 app.listen(3000, () => {
-    console.log(`API start ${port}`)
+    console.log(`API start at port ${port}`)
 })
