d2ab618db35b2f0a32bff7f5a8f2b0b07da2ce8a
Pimped Toloy <59160501@go.buu.ac.th>
2019-10-19 11:40:59 +0700

    API try-jwt v.4

--- db.js
+++ db.js
@@ -3,8 +3,7 @@ const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
 const MongoURL = process.env.MONGODB_URL
 const options = { useNewUrlParser : true, useUnifiedTopology : true}
-const database = "buu"
-const collection = "users"
+
 
 module.exports = (async () =>{
     const client = await MongoClient.connect( MongoURL, options )
--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const express = require("express")
-const mongodb = require('mongodb')
-const bcrypt = require('bcryptjs')
+const bcrypt = require("bcryptjs")
 // const client = require('./db')
 
 
 const app = express()
 const port = process.env.PORT
-const MongoClient = mongodb.MongoClient
-const MongoURL = process.env.MONGODB_URL
-const options = { useNewUrlParser : true, useUnifiedTopology : true}
 const database = "buu"
 const collection = "users"
+const connectDb = "./db"
 
 app.use(express.json())
 
@@ -27,13 +24,13 @@ app.post('/register', async (req, res)=>{
         password:encryptedPwd
     }
 
-    const client = await require('./db')
+    const client = await require(connectDb)
     // const client = await MongoClient.connect( MongoURL, options ).catch((err) => {
     //     console.log(`Cannot connect to Mongo  ${err}`)
     //     res.status(500).json({error:err})
     //     return
     // })
-    
+
     let db = client.db(database)
     let result = await db.collection(collection).insertOne(o).catch(err => {
         console.log(`Cannot insert data to users collection : ${err}`)
@@ -50,6 +47,32 @@ app.post('/register', async (req, res)=>{
     res.status(201).json(r)
 })
 
+app.post('/sign-in', async (req, res)=>{
+    let email = req.body.email
+    let password = req.body.password
+
+    const client = await require(connectDb)
+
+    let db = client.db(database)
+    let user = await db.collection(collection).findOne( { email:email } ).catch(err => {
+        console.log(`Cannot find user with email : ${err}`)
+        res.status(500).send({error:err})
+        return
+    })
+
+    if(!user){
+        res.status(401).json({error:`your given email has not been found`})
+        return
+    }
+    
+    let passwordisValid = await bcrypt.compare(password, user.password)
+    if(!passwordisValid){
+        res.status(401).json({error:`username/password is not match`})
+        return
+    }
+
+    res.status(200).json({token: '123'})
+})
 
 app.listen(port, () => {
     console.log(`API start at port ${port}`)
